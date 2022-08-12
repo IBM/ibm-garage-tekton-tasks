0697496999aa8332530e5ebe045e2c5793f3d155
Sean Sundberg <seansund@us.ibm.com>
2022-08-11 22:06:38 -0500

    Updates mirror workflow and container images
    - Updates mirror workflow process to use skopeo sync to copy all tags and simplfies credentials
    - Moves mirrored images to quay.io/cloudnativetoolkit
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- bin/mirror.sh
+++ bin/mirror.sh
@@ -3,9 +3,8 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
 ROOT_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
-DEST_USERNAME="$1"
-DEST_PASSWORD="$2"
-MIRROR_CONFIG="$3"
+MIRROR_CONFIG="$1"
+AUTH_FILE="$2"
 
 if [[ -z "${MIRROR_CONFIG}" ]]; then
   MIRROR_CONFIG="${ROOT_DIR}/mapping.txt"
@@ -16,24 +15,21 @@ if uname -a | grep -q Darwin; then
   FILTER="--override-os linux"
 fi
 
+if [[ -n "${AUTH_FILE}" ]]; then
+  AUTH_FILE_ARG="--authfile ${AUTH_FILE}"
+fi
+
 echo "Loading mirror config from ${MIRROR_CONFIG}"
 cat "${MIRROR_CONFIG}" | while read line; do
+  if [[ "${line}" =~ ^# ]]; then
+    continue
+  fi
+  
   echo "*** Processing line from config: $line"
 
   SOURCE=$(echo "$line" | sed -E "s~([^=]+)=(.*)~\1~g")
   DEST=$(echo "$line" | sed -E "s~([^=]+)=(.*)~\2~g")
 
-  if echo $SOURCE | grep -q ":"; then
-    TAGS=$(echo $SOURCE | sed -E "s/([^:]+):(.*)/\2/g")
-    SOURCE=$(echo $SOURCE | sed -E "s/([^:]+):(.*)/\1/g")
-    echo "Using source tag: ${TAGS}"
-  else
-    echo "+ Getting tags for ${SOURCE}"
-    TAGS=$(skopeo inspect ${FILTER} "docker://${SOURCE}" | jq -r '.RepoTags[.RepoTags | length] |= . + "latest" | .RepoTags | .[]' | sort -Vu | tail -5)
-  fi
-
-  for tag in ${TAGS}; do
-    echo "+ Mirroring tag docker://${SOURCE}:${tag} to docker://${DEST}:${tag}"
-    skopeo copy ${FILTER} --dest-creds "${DEST_USERNAME}:${DEST_PASSWORD}" "docker://${SOURCE}:${tag}" "docker://${DEST}:${tag}"
-  done
+  echo "+ Mirroring tag docker://${SOURCE}:${tag} to docker://${DEST}:${tag}"
+  skopeo sync ${FILTER} ${AUTH_FILE_ARG}  --src docker --dest docker "${SOURCE}" "${DEST}"
 done
